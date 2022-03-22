7299fd1442939abae50bd9d40029d024b9ab1417
GustiMuhammadAdzaky <xxzakixx001@gmail.com>
2022-03-22 12:06:38 +0700

    Floating + Empty State

--- app/src/main/java/com/zaky/myapplication/SecondActivity.kt
+++ app/src/main/java/com/zaky/myapplication/SecondActivity.kt
@@ -1,15 +1,15 @@
 package com.zaky.myapplication
 
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class SecondActivity : AppCompatActivity() {
@@ -25,18 +25,36 @@ class SecondActivity : AppCompatActivity() {
         rvGridNabi = findViewById(R.id.rv_horizontal)
         list.addAll(RvData.listData)
 
+        val btn_click_me = findViewById(R.id.btn_bookmark) as FloatingActionButton
+
         showRecyclerCardView()
 
+
+//        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
         supportActionBar?.hide()
 
+
+
+
+        btn_click_me.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            Toast.makeText(this@SecondActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+
+
+
+
 
     private fun showRecyclerCardView() {
 //        rvGridNabi.layoutManager = LinearLayoutManager(this)
         rvGridNabi.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         val viewHorizontalAdapter = ViewHorizontalAdapter(list)
         rvGridNabi.adapter = viewHorizontalAdapter
-        viewHorizontalAdapter.setOnItemClickCallback(object : ViewHorizontalAdapter.OnItemClickCallback {
+        viewHorizontalAdapter.setOnItemClickCallback(object :
+            ViewHorizontalAdapter.OnItemClickCallback {
             override fun onItemClicked(data: Nabi) {
 //                showSelectedSkinCare(data)
             }
@@ -45,7 +63,16 @@ class SecondActivity : AppCompatActivity() {
 
 
 
+//    fun buttonPindahGambar(view: View) {
+//        btn_click_me.
+//    }
+
+
+
+
+
     fun undoButton(view: View) {
         finish()
     }
+
 }
